302d9e821914064d55093629222004e51cf224ec
gifflet <guisousa09@hotmail.com>
2024-11-15 01:24:38 -0300

    feat: add caching layer with KV namespace for GitHub API responses

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,28 @@
 import { Hono } from "hono";
 
-type Bindings = {}
+type Bindings = {
+	CACHE: KVNamespace
+}
 
 const app = new Hono<{ Bindings: Bindings }>();
 
 app.get("/:username", async c =>  {
 	const username = c.req.param("username");
+	const cachedResponse = await c.env.CACHE.get(username, "json");
+	
+	if (cachedResponse) {
+		return c.json(cachedResponse)
+	}
+	
 	const response = await fetch(`https://api.github.com/users/${username}/repos`, {
 		headers: {
 			"User-Agent": "CF-Worker"
 		}
 	})
+
 	const data = await response.json();
-	return c.json(data);
+	await c.env.CACHE.put(username, JSON.stringify(data));
+	return c.json(data);	
 });
 
 export default app
