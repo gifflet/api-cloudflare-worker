227a01469a4f561974457bae6d91a2fe20386e9b
gifflet <guisousa09@hotmail.com>
2024-11-15 00:55:57 -0300

    feat: integrate 'hono' library for user repository API endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Hono } from "hono";
 
-export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+type Bindings = {}
+
+const app = new Hono<{ Bindings: Bindings }>();
+
+app.get("/:username", async c =>  {
+	const username = c.req.param("username");
+	const response = await fetch(`https://api.github.com/users/${username}/repos`, {
+		headers: {
+			"User-Agent": "CF-Worker"
+		}
+	})
+	const data = await response.json();
+	return c.json(data);
+});
+
+export default app
